196a0512581fab8d819a260e41b98d8d98898ea5
Amanda <jkim@lcs.on.ca>
2020-06-03 19:27:49 -0400

    yoooooooo I got it to draw all 5 lines at the same time equal space apart with the new counter var. Don't know why it's only going right then left. And also just stopping before going off the canvas

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -19,16 +19,22 @@ class Sketch : NSObject {
         // No fill on canvas
         canvas.drawShapesWithFill = true
         
+        // This will come in handy to move the
+        var counter = 0
+        
         // Create many instances of the Agent structure
         for _ in 1...5 {
             
-            let anotherAgent = Agent(centre: Point(x: canvas.width / 2, y: 600),
+            let anotherAgent = Agent(centre: Point(x: canvas.width / 2, y: 600 - (120 * counter)),
                                      radius: 30,
-                                     velocity: Vector(x: Double.random(in: -2...2),
+                                     velocity: Vector(x: Double.random(in: 2...2),
                                                       y: 0 ),
                                      drawsUpon: canvas)
             
+            counter += 1
+            
             agents.append(anotherAgent)
+            
             
         }
         
