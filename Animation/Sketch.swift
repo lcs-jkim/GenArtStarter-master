a2d06129a9813e2cc6773c391d9e52ddd58b1619
Amanda <jkim@lcs.on.ca>
2020-06-08 14:52:28 -0400

    Yasss we got the first red line to go from centre to top right corner. Added variables right, left, up, down and it constantly being added to in draw func for easy use to dictate direction of the line

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,6 +7,11 @@ class Sketch : NSObject {
     //       Therefore, the line immediately below must always be present.
     let canvas: Canvas
     
+        var right = 350
+        var left = 350
+        var up = 350
+        var down = 350
+    
     // This function runs once
     override init() {
         
@@ -21,8 +26,17 @@ class Sketch : NSObject {
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-       
-     
+        
+        right += 2
+        left -= 2
+        up += 2
+        down -= 2
+        
+        // Starting with the red line going right and up
+        canvas.fillColor = Color.red
+        
+        // We now start with a circle in the middle of the canvas
+        canvas.drawEllipse(at: Point(x: right, y: up), width: 60, height: 60)
     }
    
 }
