3d2b04cf3027f296042fdd556442333f41124f6c
Amanda <jkim@lcs.on.ca>
2020-06-10 20:10:29 -0400

    deleted all the colors I added. Will add back if I need them

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -16,12 +16,6 @@ class Sketch : NSObject {
         
         // Our funky brick color and flower colors
         let brick = Color(hue: 9, saturation: 60, brightness: 50, alpha: 100)
-        let pink = Color(hue: 324, saturation: 50, brightness: 75, alpha: 100)
-        let red = Color(hue: 360, saturation: 60, brightness: 52, alpha: 100)
-        let darkPurple = Color(hue: 298, saturation: 50, brightness: 30, alpha: 100)
-        let lightPurple = Color(hue: 279, saturation: 50, brightness: 85, alpha: 100)
-        let lightBlue = Color(hue: 225, saturation: 50, brightness: 80, alpha: 100)
-        let orange = Color(hue: 35, saturation: 90, brightness: 80, alpha: 100)
         let green = Color(hue: 107, saturation: 50, brightness: 38, alpha: 100)
         
         // Draws the fun flower pot, starting with bottom
